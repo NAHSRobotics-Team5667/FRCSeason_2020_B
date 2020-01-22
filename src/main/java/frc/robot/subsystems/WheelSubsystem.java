bad1c2f657f45fda4bf68209a741af84f8d95edf
Olu Fasoro <fasoro.1@napls.us>
2020-01-22 17:45:48 -0500

    edited WheelSubsystem slightly

--- src/main/java/frc/robot/subsystems/WheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/WheelSubsystem.java
@@ -37,11 +37,14 @@ public class WheelSubsystem extends SubsystemBase {
    *  @param speed : Speed that the wheel motor moves
   */
   public void shoot(double speed) {
-    this.m_motor.set(speed);
+    m_motor.set(speed);
   }
   
+  /**
+  * Stops the wheel motor
+  */
  public void turnOffMotor() {
-   RobotContainer.stopMotor()
+   m_motor.stopMotor()
  }
   @Override
   public void periodic() {
